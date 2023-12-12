d170cdb76392c0de770b2d6b6d64e44456e05048
wctn <wichittranut.b@iixp.co.th>
2023-12-12 17:28:47 +0700

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DxButtonModule, DxFormModule, DxSelectBoxModule, DxTextAreaModule } from 'devextreme-angular';
@@ -19,7 +19,8 @@ import { RegisterComponent } from './page/register/register.component';
     DxFormModule,
     DxButtonModule,
     DxTextAreaModule,
-    DxSelectBoxModule
+    DxSelectBoxModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,20 +1,49 @@
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import {  Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  public loginForm!: FormGroup
+  constructor(private http: HttpClient,private router: Router) { }
 
-  constructor() { }
+login = {
+    "OrganizationId": 1,
+    "UserName": "sysadmin",
+    "Password": "SpApp@09",
+}
+  loginClick(){
+    // const headers = { 'Content-Type': 'application/json','Access-Control-Allow-Origin':'*' };
+    return this.http.post('http://pg13sql.anykeeps.com/Account/Login',this.login)
+    .subscribe(
+        res =>{
+            console.log(res);
+        },
+        err => {
+            console.log(err.message);
+        }
+    )
+  //   this.http.post<any>('http://pg13sql.anykeeps.com/healthnextapi2/Account/Login',{
+  //     "OrganizationId": 1,
+  //     "UserName": "sysadmin",
+  //     "Password": "SpApp@09",
+  // },{ 'headers': headers})
+  //   .subscribe(res=>{
+  //     if(true){
+  //       alert(res)
+  //     }
+  //     else{
+  //       alert('Unsuccess')
+  //     }
+  //   })
 
-  ngOnInit() {
-  }
 
-  click(e : any)
-  {
-
-  }
-
+}
 }
